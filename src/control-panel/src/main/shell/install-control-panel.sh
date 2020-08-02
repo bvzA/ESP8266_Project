86b25ff1fd464b2e5e2acd72fbbb3bbf17c48f0e
Phanomphon Yotchon <phanomphon.yotchon@gmail.com>
2020-08-02 23:49:17 +0700

    v0.3

--- src/control-panel/src/main/shell/install-control-panel.sh
+++ src/control-panel/src/main/shell/install-control-panel.sh
@@ -4,7 +4,7 @@ curl -L https://github.com/bvzA/ESP8266_Project/releases/download/v0.2-alpha/con
 mv control-panel /opt/control-panel
 
 mkdir -p /etc/control-panel
-cp /opt/control-panel/application-template.properties /etc/control-panel/application.conf
+cp /opt/control-panel/application-template.properties /etc/control-panel/application.properties
 
 # service control-panel start/stop/status 
 ln -s /opt/control-panel/cpanel /etc/init.d/control-panel
