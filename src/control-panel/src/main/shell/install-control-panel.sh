c44c9b077338d362d7d1b699f33783824281e791
Phanomphon Yotchon <phanomphon.yotchon@gmail.com>
2020-08-03 21:47:16 +0700

    v0.4-alpha

--- src/control-panel/src/main/shell/install-control-panel.sh
+++ src/control-panel/src/main/shell/install-control-panel.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-curl -L https://github.com/bvzA/ESP8266_Project/releases/download/v0.3-alpha/control-panel.tar.gz | tar xzv
+curl -L https://github.com/bvzA/ESP8266_Project/releases/download/v0.4-alpha/control-panel.tar.gz | tar xzv
+
+rm -rf /etc/systemd/system/control-panel.service
+rm -rf /etc/init.d/control-panel
+rm -rf /opt/control-panel
+
 mv control-panel /opt/control-panel
 
 mkdir -p /etc/control-panel
-cp /opt/control-panel/application-template.properties /etc/control-panel/application.properties
+cp -f /opt/control-panel/application-template.properties /etc/control-panel/application-template.properties
 
-# service control-panel start/stop/status 
+# service control-panel start/stop/status (System V Init)
 ln -s /opt/control-panel/cpanel /etc/init.d/control-panel
 
-# systemctl start/stop/status control-panel.service
+# systemctl start/stop/status/reload/restart control-panel.service (Systemd)
 ln -s /opt/control-panel/control-panel.service /etc/systemd/system/control-panel.service
